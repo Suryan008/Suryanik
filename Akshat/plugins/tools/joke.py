860c8634ba4ac690a7f76e8a6aabc0a75901d4ca
Suryan008 <157453764+Suryan008@users.noreply.github.com>
2024-03-07 01:22:39 +0530

    Update joke.py

--- Akshat/plugins/tools/joke.py
+++ Akshat/plugins/tools/joke.py
@@ -2,7 +2,7 @@ import requests
 from Akshat import app
 from pyrogram import Client, filters
 
-JOKE_API_ENDPOINT = 'https://hindi-jokes-api.onrender.com/jokes?api_key=1a6d440e3f5971eecebceee818c2'
+JOKE_API_ENDPOINT = 'https://v2.jokeapi.dev/joke/Miscellaneous?amount=10'
 
 @app.on_message(filters.command("hjoke"))
 async def joke(_, message):
