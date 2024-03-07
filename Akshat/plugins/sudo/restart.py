bb9218bb4f30917a49ffcd529eeaccb968cb0c4f
Suryan008 <157453764+Suryan008@users.noreply.github.com>
2024-03-07 11:14:15 +0530

    Update restart.py

--- Akshat/plugins/sudo/restart.py
+++ Akshat/plugins/sudo/restart.py
@@ -27,7 +27,7 @@ async def is_heroku():
     return "heroku" in socket.getfqdn()
 
 
-@app.on_message(filters.command(["getlog", "logs", "getlogs","dedo"], prefixes=["/", "!", "%", ",", "", ".", "@", "#","janu"]) & SUDOERS)
+@app.on_message(filters.command(["getlog", "logs", "getlogs","dedo"], prefixes=["/", "!", "%", ",", "", ".", "@", "#","ammu"]) & SUDOERS)
 @language
 async def log_(client, message, _):
     try:
